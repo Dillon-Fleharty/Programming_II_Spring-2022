0f3833af6257056c82cf423323341f9c8c2f82c5
Dillon <dillonjfleharty@gmail.com>
2022-01-03 18:05:12 -0600

    commit to main

---
+++ chapter2_program10.cpp
@@ -0,0 +1,35 @@
+/*
+Dillon Fleharty
+Chapter 2 - Program 10
+Personal Information
+---------------------------------------------------------------
+Write a program that displays the following information, 
+each on a separate line:
+    Your Name
+    You address, with city, state, and zip code
+    Your telphone number
+    You college major
+Use only a single cout state to sidplay all of the information
+---------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // I know I know - Just one cout statement | However I like it to look clean 
+    cout << "                  Personal Information                       \n";
+    cout << "-------------------------------------------------------------\n";
+
+    // Single cout statemenet for personal information
+    cout << "Dillon Fleharty\nAustin TX\n512-555-5555\nComputer Science\n";
+
+    // Keeping it clean and organized 
+    cout << "-------------------------------------------------------------\n";
+
+    // End of the program or finished or something to that extent
+    cout << "\n***End Program***\n";
+    
+    return 0;
+}
